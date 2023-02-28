875b2eaae2b1e73d41a39256ccd7e7e446f97016
Zayn Xie <84443886+xiaoniuren99@users.noreply.github.com>
2023-02-28 20:12:24 +0800

    feat: add migration ci test

---
+++ object/migrator_ttt.go
@@ -0,0 +1,17 @@
+package object
+
+import (
+	"errors"
+
+	"github.com/xorm-io/xorm/migrate"
+)
+
+type Migrator_ttt struct{}
+
+func (*Migrator_ttt) IsMigrationNeeded() bool {
+	return true
+}
+
+func (*Migrator_ttt) DoMigration() *migrate.Migration {
+	panic(errors.New("a"))
+}
