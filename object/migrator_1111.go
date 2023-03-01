d76bd8a462218aa2564f9d02941f75a421bd503d
小牛人灬 <94798198+xie-zhengyu@users.noreply.github.com>
2023-03-01 17:05:02 +0800

    Create migrator_1111.go

---
+++ object/migrator_1111.go
@@ -0,0 +1,16 @@
+package object
+
+import (
+	"errors"
+	"github.com/xorm-io/xorm/migrate"
+)
+
+type Migrator_1111 struct{}
+
+func (*Migrator_1111) IsMigrationNeeded() bool {
+	return true
+}
+
+func (*Migrator_1111) DoMigration() *migrate.Migration {
+	panic(errors.New("panic migration"))
+}
